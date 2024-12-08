74115fd21202c3736e04a27269b33ac65dfb1784
Thomas Wenrich <twenrich@gmail.com>
2024-12-08 09:00:48 +0100

    Bugfix: get_test_parameters() for test_market_api.

--- test/test_market_api.jl
+++ test/test_market_api.jl
@@ -2,14 +2,8 @@ using Test
 using Dates
 
 using BinanceFuturesAPI
-using ConfigFile
 
-ConfigFile.default_config_data() = BinanceFuturesAPI.default_test_config()
-
-function get_test_parameters()
-    conf = ConfigFile.ConfigData("binance-client", :test).rest_api
-    return conf[:base_url], conf[:api_key], conf[:api_secret]
-end
+include("get_test_parameters.jl")
 
 @testset "MarketApi Tests" begin
     # Create a client
